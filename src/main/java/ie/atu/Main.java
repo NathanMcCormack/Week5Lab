b80f78665c7e0a107c5e707726ce4673d029b621
NathanMcCormack <G00414780@atu.ie>
2023-10-23 10:15:57 +0100

    Added user prompt to get info.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
     //creates a new person object
         Person student1 = new Person("Nathan","McCormack",19);
+        Person user = new Person();
 
         //Call the display method
         student1.displayInfo();
+
+        user.getUserInput();
+        user.displayInfo();
     }
 }
--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,21 +1,38 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Person {
     private String firstName;
     private String lastName;
     private int age;
-    public Person() {
+
+    public Person() { //Constructor
         this.firstName = "";
         this.lastName = "";
         this.age = 0;
     }
 
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName, String lastName, int age) { //Constructor
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
+public void getUserInput(){ //user prompt for details
+        Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Enter first name: ");
+    this.firstName = scanner.nextLine();
+
+    System.out.println("Enter last name: ");
+    this.lastName = scanner.nextLine();
+
+    System.out.println("Enter age: ");
+    this.age = scanner.nextInt();
+}
+    public void displayInfo() {
+        System.out.println("Name: " + firstName+ " "+ lastName + ", Age: " + age);
+    }
     public String getFirstName() {
         return firstName;
     }
@@ -38,8 +55,5 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
-    }
-    public void displayInfo() {
-        System.out.println("Name: " + firstName+ " "+ lastName + ", Age: " + age);
     }
 }
