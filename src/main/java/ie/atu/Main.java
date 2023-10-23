4f7b809b6672bcc3ba8865db677860d52730ad38
NathanMcCormack <G00414780@atu.ie>
2023-10-23 10:00:14 +0100

    Added a new person object and display method in Main. Added getters and setters in the Person Class

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -2,6 +2,10 @@ package ie.atu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //creates a new person object
+        Person student1 = new Person("Nathan","McCormack",19);
+
+        //Call the display method
+        student1.displayInfo();
     }
 }
